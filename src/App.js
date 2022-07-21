253bb427ac759becf34fea62c6984e2b650928e2
Subham Subhasis Patra <subhasispatra94@gmail.com>
2022-07-21 09:51:21 +0530

    Fixed The CSS

--- src/App.js
+++ src/App.js
@@ -27,6 +27,10 @@ function App() {
     DifficultyColor = '#CD0000';
   }
 
+  let scoreColor = 'lightBlue';
+  let mainbg = 'white';
+  let sideColor = 'white';
+
   useEffect(() => {
     const ticker = setTimeout(() => setSeconds(seconds + 1), 1000);
     return () => clearTimeout(ticker);
@@ -67,79 +71,84 @@ function App() {
   };
 
   return (
-    <div className="App container">
-      {/* 1. Header  */}
-      <h1>AI Learning</h1>
+    <div style={{background : `${mainbg}`}}>
+      <div className="App container" style={{ padding: "5px", background : `${sideColor}` }}>
 
-      {/* 2. Current Score  */}
-      {/* <h2>Score: {score}</h2>
-      <h2>question Level : {questionLevel}</h2> */}
 
-      {/* 2.1 Time elapsed  */}
-      {stopTimer ? (
-        <h2>Total Time : {totalTime}</h2>
-      ) : (
-        <h2>Time Elapsed: {seconds}</h2>
-      )}
-
-      <div class="card-group">
-        <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Score</h5>
-              <p class="card-text">{score}</p>
-            
-            </div>
-        </div>
-        <div class="card" style={{background:`${DifficultyColor}`}}>
-            <div class="card-body" >
-              <h5 class="card-title">Difficulty Level</h5>
-              <p class="card-text" >{questionLevel}</p>
-            </div>
-        </div>
-      </div>
-
-      {/* 3. Show results or show the question game  */}
-      {showResults ? (
-        /* 4. Final Results */
-        <div className="card">
-          <h1>Final Results</h1>
-          <h2>
-            {score} out of {Questions[questionLevel].length} correct - (
-            {(score / Questions[questionLevel].length) * 100}%)
-          </h2>
-          <h2>Total time taken {totalTime}</h2>
-          <button onClick={() => restartGame()}>Restart game</button>
-        </div>
-      ) : (
-        /* 5. Question Card  */
-        <div className="card">
-          {/* Current Question  */}
+        <div className="card" style={{ padding: "5px", background : `${sideColor}` }}>
           <div className="card-body">
-            <h2>
-              Question: {currentQuestion + 1} out of{" "}
-              {Questions[questionLevel].length}
-            </h2>
-            <h3 className="question-text">
-              {Questions[questionLevel][currentQuestion].text}
-            </h3>
+            {/* 1. Header  */}
+            <h1>AI Learning</h1>
+            {/* 2.1 Time elapsed  */}
+            {stopTimer ? (
+              <h2>Total Time : {totalTime}</h2>
+            ) : (
+              <h2>Time Elapsed: {seconds}</h2>
+            )}
           </div>
+          <div class="card-group" style={{ padding: "5px" , background : `${sideColor}` }}>
+            <div class="card" style={{ background: `${scoreColor}`, padding: "5px" }}>
+              <div class="card-body">
+                <h5 class="card-title">Score</h5>
+                <p class="card-text">{score}</p>
 
-          {/* List of possible answers  */}
-          <ul className="list-group list-group-flush">
-            {Questions[questionLevel][currentQuestion].options.map((option) => {
-              return (
-                <li
-                  className="list-group-item"
-                  key={option.id}
-                  onClick={() => optionClicked(option.isCorrect)}
-                >
-                  {option.text}
-                </li>
-              );
-            })}
-          </ul>
+              </div>
+            </div>
+            <div class="card" style={{ background: `${DifficultyColor}`, padding: "5px" }}>
+              <div class="card-body" >
+                <h5 class="card-title">Difficulty Level</h5>
+                <p class="card-text" >{questionLevel}</p>
+              </div>
+            </div>
+          </div>
         </div>
-      )}
+
+        {/* 3. Show results or show the question game  */}
+        {showResults ? (
+          /* 4. Final Results */
+          <div className="card">
+            <h1>Final Results</h1>
+            <h2>
+              {score} out of {Questions[questionLevel].length} correct - (
+              {(score / Questions[questionLevel].length) * 100}%)
+            </h2>
+            <h2>Total time taken {totalTime}</h2>
+            <button onClick={() => restartGame()}>Restart game</button>
+          </div>
+        ) : (
+          /* 5. Question Card  */
+          <div className="card" style={{ padding: "5px", background : `${sideColor}` }}>
+            {/* Current Question  */}
+            <div className="card-body">
+              <h2>
+                Question: {currentQuestion + 1} out of{" "}
+                {Questions[questionLevel].length}
+              </h2>
+              <h3 className="question-text">
+                {Questions[questionLevel][currentQuestion].text}
+              </h3>
+            </div>
+
+            {/* List of possible answers  */}
+            <div >
+            <ul className="list-group list-group-flush" style={{padding: "5px"}}>
+              {Questions[questionLevel][currentQuestion].options.map((option) => {
+                return (
+                  <li
+                  style={{background : '#7D9D9C', padding: "5px"}}
+                    className="list-group-item"
+                    key={option.id}
+                    onClick={() => optionClicked(option.isCorrect)}
+                  >
+                    {option.text}
+                  </li>
+                );
+              })}
+            </ul>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
